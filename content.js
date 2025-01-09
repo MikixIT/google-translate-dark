0c08a44945093d2fed23f88aded45a856e221fe3
Michael Torres <michael.46@live.it>
2025-01-09 20:32:00 +0100

    typo everywhere

--- content.js
+++ content.js
@@ -2,5 +2,5 @@ document.body.style.background = "black";
 document.querySelector(".gb_kd.gb_od.gb_Fd.gb_ld").style.background = "black";
 
 console.log(
-  "Thanks for using my extension for Google Traslate, here the link for update and more. -> https://github.com/MikixIT/google-traslate-dark"
+  "Thanks for using my extension for Google Translate by MikixIT, here the link for update and more. -> https://github.com/MikixIT/google-translate-dark  [FOREVER FREE AND ADS FREE]"
 );
